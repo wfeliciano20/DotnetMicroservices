7b6f8c8960d7b1efed6775fa49e31ec63d3ecc6d
wfeliciano20 <william.feliciano2@gmail.com>
2024-02-15 22:14:13 -0500

    Added  Gateway and 2 Events

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -34,15 +34,7 @@ builder.Services.AddMassTransit(x =>
     });
 });
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.Authority = builder.Configuration["IdentityServiceUrl"];
-        options.RequireHttpsMetadata = false;
-        options.TokenValidationParameters.ValidateAudience = false;
-        options.TokenValidationParameters.NameClaimType = "username";
 
-    });
 
 var app = builder.Build();
 app.UseAuthentication();
